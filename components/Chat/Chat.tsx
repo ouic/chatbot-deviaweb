1199537a6dc30b913985af6227f0d5d1176c88de
eric pinto <eric.pinto@hotmail.fr>
2023-06-06 03:10:01 +0200

    update

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,22 +352,20 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
+            Deviaweb IA
           </div>
           <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
+            <div className="mb-8">Deviaweb Chatbot est un projet open source de clone de ChatGPT de OpenAI propulsé par <a href="https://github.com/mckaywrigley/chatbot-ui"><u>Chatbot UI</u></a>.</div>
             <div className="mb-2 font-bold">
-              Important: Chatbot UI is 100% unaffiliated with OpenAI.
+              Important: Deviaweb Chatbot est 100% non-affilié avec OpenAI.
             </div>
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
             <div className="mb-2">
-              Chatbot UI allows you to plug in your API key to use this UI with
-              their API.
+              Deviaweb Chatbot vous permet d'entrer votre clé API pour utiliser cette interface
             </div>
             <div className="mb-2">
-              It is <span className="italic">only</span> used to communicate
-              with their API.
+              <span className="italic"><u>uniquement</u></span> avec l'API d'OpenAI.
             </div>
             <div className="mb-2">
               {t(
@@ -375,7 +373,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
               )}
             </div>
             <div>
-              {t("If you don't have an OpenAI API key, you can get one here: ")}
+              {t("Si vous n'avez pas de clé API, vous pouvez en obtenir une ici : ")}
               <a
                 href="https://platform.openai.com/account/api-keys"
                 target="_blank"
@@ -405,7 +403,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : (
-                      'Deviaweb Chatbot'
+                      'Deviaweb IA'
                     )}
                   </div>
 
